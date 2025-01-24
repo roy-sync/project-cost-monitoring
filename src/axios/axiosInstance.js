6e948e7b6beac24461804023d62694f4e1585b93
Roy Johairi Lucman <lucman.royjohairi@gmail.com>
2025-01-24 16:14:15 +0800

    fix: issues with dashboard

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,14 +1,7 @@
 import axios from "axios";
-import https from "https";
 
 const axiosInstance = axios.create({
   baseURL: "http://206.189.147.71:10100/api",
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false, // Disable SSL certificate validation
-  }),
-  headers: {
-    'Access-Control-Allow-Origin': '*',  // Allow cross-origin requests from any origin
-  },
 });
 
 axiosInstance.interceptors.request.use(
