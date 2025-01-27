ad2beb5faaaee95b10155021d35fb08470a00b2b
Roy Johairi Lucman <lucman.royjohairi@gmail.com>
2025-01-27 16:17:26 +0800

    chore: revert changes

--- src/app/bitrix/callback/route.ts
+++ src/app/bitrix/callback/route.ts
@@ -6,7 +6,7 @@ import fetch from 'node-fetch';
 // const CLIENT_SECRET = process.env.BITRIX_CLIENT_SECRET!;
 const CLIENT_ID = "local.674e7e4d5f4a02.57385705";
 const CLIENT_SECRET = "jHxd0SvBLYa9GAEwTu06JxYX35TW5nhrJfX7cRqvJ0oX9ELkpH";
-const REDIRECT_URI = 'http://206.189.147.71:54030/bitrix/callback';
+const REDIRECT_URI = 'https://project-cost-monitoring.vercel.app/bitrix/callback';
 
 // Define an interface for the expected structure of token data
 interface TokenData {
@@ -51,7 +51,7 @@ export async function GET(request: Request) {
             // Set token as an HTTP-only cookie in the response headers
             const origin = new URL(request.url).origin;
             // const testResponse = NextResponse.redirect(`${origin}/`);
-            const response = NextResponse.redirect("http://206.189.147.71:54030/");
+            const response = NextResponse.redirect("https://project-cost-monitoring.vercel.app/");
 
             // console.log(testResponse);
 
